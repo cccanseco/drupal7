e5cb809ce94e8f91ff4f8321821025426374fa7c
cccanseco <carlos.cuadrado@grupoonetec.com>
2016-04-26 10:47:33 +0200

    prueba 3 para ver si llegamos a Drupal
    prueba 3 para ver si llegamos a Drupal

--- Test.php
+++ Test.php
@@ -2,7 +2,7 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-            $miBlog = BlogTestCase();
+            //$miBlog = BlogTestCase();
             //$this->assertEquals($miBlog->testBlogLLegaTest(), true);
             $this->assertEquals(TRUE, TRUE);
   	}
