d5d1d3231e7304ee236ccab674b26804dfe0c69d
cccanseco <carlos.cuadrado@grupoonetec.com>
2016-04-26 13:03:25 +0200

    Prueba 4 para llegar a Drupal
    Prueba 4 para llegar a Drupal

--- Test.php
+++ Test.php
@@ -2,9 +2,8 @@
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
-            //$miBlog = BlogTestCase();
             //$this->assertEquals($miBlog->testBlogLLegaTest(), true);
-            $this->assertEquals(TRUE, TRUE);
+            $this->assertEquals(testBlogLLegaTest(), TRUE);
   	}
 }
 ?>
